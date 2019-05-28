4a617f148837b43816fab9ea1a2a6653aaf0d9e7
tuabuela23 <cristian-317@hotmail.es>
2019-05-27 21:22:46 -0500

    object

---
+++ clases/Persona.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+class Persona{
+    public $idPersona;
+    public $idTipoDocumento;
+    public $documentoPersona;
+    public $nombrePersona;
+    public $apellidoPersona;
+    public $correoPersona;
+    public $telefonoPersona;
+    public $avatarPersona=null;
+    public $direccionPersona;
+}
+
+?>
