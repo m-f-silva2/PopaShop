ca2d9a7286fa91a241fe3cdd11c8f5cbad64b69f
tuabuela23 <cristian-317@hotmail.es>
2019-05-28 10:09:42 -0500

    text

--- control/logica/PersonaConsult.php
+++ control/logica/PersonaConsult.php
@@ -7,7 +7,7 @@ $pass=$_REQUEST['password'];
 $rs=mysqli_query($conn,"select * from persona where idPersona=1");
 $idPersona;
 
-
+//
     $datos=array();
 
 while($row = mysqli_fetch_array($rs,MYSQLI_ASSOC)){
