97c157e76fc9be4d5d8ffcba7dc6a4b1a7da7e2a
tuabuela23 <cristian-317@hotmail.es>
2019-05-29 18:44:42 -0500

    traer Todos los productos

--- control/logica/ProductosGet.php
+++ control/logica/ProductosGet.php
@@ -5,13 +5,9 @@
 
 class ProductosGet{
 
-	private $usuario;
-	private $password;
-
 	//Constructor donde recibe los datos del formulario como el usuario y la contraseña
-	public function __construct($usuario,$password){
-		$this->usuario = $usuario;
-		$this->password = $password;
+	public function __construct(){
+		
 		//Aqui si se puede acceder a esta funcion de tipo private.
 		$this->validarProductos();
 	}
@@ -28,13 +24,11 @@ class ProductosGet{
 	//Validar datos enviados del formulario con los de la base de datos.
 	public function validarProductos(){
 
-		if(isset($this->usuario) && isset($this->password)){
-			$tabla = "usuario";
+		
+			$tabla = "productos";
 			$conn = new \PDO('mysql:host=localhost;dbname=popashop','root','');
-			$stmt = $conn->prepare("SELECT idRol FROM $tabla WHERE login = :item AND password = :item2");
+			$stmt = $conn->prepare("SELECT * FROM $tabla");
 
-			$stmt->bindParam(":item", $this->usuario, \PDO::PARAM_STR);
-			$stmt->bindParam(":item2", $this->password, \PDO::PARAM_STR);
 			$stmt->execute();
 			if ($stmt) {
 				return $stmt->fetch();
@@ -51,7 +45,7 @@ class ProductosGet{
                         }//}  
                         echo json_encode($datos); // aqui se guardan todos los productos en JSON);
 	
-		}
+		
 	}
 }
 ?>
