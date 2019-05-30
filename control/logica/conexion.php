a914c634cdaece75c8ad98ec082671aa4755957e
Manuel <mfsilva@unimayor.edu.co>
2019-05-30 18:14:45 -0500

    conexion.php
    Se añade la clase conexion y el metodo(function) conectar.

--- control/logica/conexion.php
+++ control/logica/conexion.php
@@ -1,19 +1,22 @@
-
-<?php
-$servername = "localhost";
+<?php  namespace Logica;
+/*$servername = "localhost";
 $database = "popashop";
 $username = "root";
 $password = "";
 
 //$Persona= new Persona;
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = \mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 
 }
-
-
-
+*/
+class Conexion{
+	public function conectar(){
+		$conn = new \PDO('mysql:host=localhost;dbname=popashop','root','');
+		return $conn;
+	}
+}
 ?>
