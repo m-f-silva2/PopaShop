baa20db91fd56e7a28cbb17a1a03b84a29f325bf
Manuel <mfsilva@unimayor.edu.co>
2019-06-01 12:27:33 -0500

    Views
    Cambio de carpeta Interfaces a Views

---
+++ views/cliente/inicio-cliente.php
@@ -0,0 +1,39 @@
+<div class="row" id="row1">
+    <div class="col-md-12">
+            <div id="tablaProductos" align="center">
+              <h4>Productos</h4>
+               
+              
+              
+              
+              
+                <?php 
+                
+                
+                require "control/logica/ProductosGet.php";
+                /*
+                $datoProductos = Controller\ProductoController::ctrMostrarProducto();
+                //var_dump($datoProductos);
+                foreach ($datoProductos as $dato) {
+                    echo '<div class="col-md-2" id="prodCliente">
+                        <div  id="prodClienteDivImagen">
+                            <img class="img-circle img-responsive" src="src/assets/productos/' . $dato["fotoProducto"].'" alt="imagen de '.$dato["nombreProducto"].'" id="prodClienteImagen">
+                        </div>
+                        <div>
+                            <h3>'.$dato["nombreProducto"].'</h3>
+                        </div>
+                        <div>
+                            <h4>$'.$dato["precioProducto"].'</h4>
+                        </div>
+                        <div>
+                            
+                        </div><br>
+                        </div>';
+                } */
+                ?>
+                
+            </div>
+        </div>
+        </div><br>
+    </div>
+</div>
