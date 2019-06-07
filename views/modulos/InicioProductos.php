ce5425ad90119d2a29179f5e4bfdf9018e946310
efom444 <efortiz@unimayor.edu.co>
2019-06-07 00:00:08 -0500

    Arreglado

--- views/modulos/InicioProductos.php
+++ views/modulos/InicioProductos.php
@@ -1,3 +1,7 @@
+<link type="text/css" rel="stylesheet" href="src/css/inicioProductos.css">
+    <!-- =====================================
+                    BUSCADOR
+    ======================================-->
 <form id="buscador" class="box" name="buscador" method="post" action="">
     <div class="container-1">
         <input id="search" name="buscar" type="search" placeholder="Buscar aquí..." autofocus>
@@ -10,58 +14,10 @@
     <div class="col-md-12" style="background-color: #eceff2">
         <div class="content-wrapper">
             <section class="content-header">
-
-<div class="row" id="row1" align="center">
-    <div class="col-md-12" style="background-color: #eceff2" align="center">
-        <div class="content-wrapper"align="center">
-        <section class="content-header" >
-   
-              <table class="table table-responsive table-hover" id="tablaProductos">
-                  <thead style="background-color: #eceff2;">
-                      <!--=====================================
-        estilo de cajon productos
-        ======================================-->
-                     <style>
-                .contenedorProductos{ margin: auto;width:220px;height: 220px;float: left;margin-right: 50px;margin-bottom: 20px;border: 1px solid #000; background-color: #FFFFFF}
-                ul{list-style: none;margin: 0px; padding: 0px; }
-                ul li{width: 150px; }
-               .botonDetalle{background-color: #4CAF50; /* Green */border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none; display: inline-block;font-size: 16px;}
-            </style>
-            
-                  
-                  </thead>
-                     <!--=====================================
-       CATEGORIAS
-        ======================================-->
-                  <h3>Tienda Online</h3>
-                  <br>
-                  
-                  <tbody id="tblCatego">
-                  
-                      <select style="width: 205px;" type="number" class="form-control" placeholder="Tipo Producto" name="tipoProducto" id="tipoProducto" >
-                         
-                          <option id="descripcionProducto" value="null">Filtrar:<?php 
-                          require_once "control/logica/ProductosGet.php";
-                          $datoCategorias = Logica\ProductosGet::categorias();
-                          foreach ($datoCategorias as $dato1) {
-                          $descripcion=$dato1["descripcionProducto"];
-                           $idTipo=$dato1["idTipoProducto"];
-                          echo "<option value='$idTipo'>".$descripcion."</option>";
-                          }
-                          ?></option>
-                       </select>
-                      <input  type="submit" onclick="return catego()"value="Enviar">
-                      
-                      <script>
-                      function catego(){
-                          <?php 
-                            if(@$_POST['tipoProducto'] != null){
-
+    
                 <!--=====================================
                                 CATEGORIAS
                 ======================================-->
-
-                
                 <div class="categoriasbox" >
                     <h3>Tienda Online</h3>
                     <div class="filtrar">
@@ -92,69 +48,62 @@
                             }
 
                             ?>
-                      alert("buscarCategoria");
-                       <ul class="nav navbar-nav">
-                          <li><a id="a"  href="InicioProductos">InicioProductos</a></li>
-                          </ul>
-                      }
-                     
-                      </script>
-                         
-                  </tbody>
-                  
-                  <br>
-                  </table>
-                  <table class="table table-responsive table-hover" id="tablaProductos">
-                         <!--=====================================
-        BUSCADOR
-        ======================================-->
-                      <form id="buscador" name="buscador" method="post" action=""> 
-                          <input id="buscar" name="buscar" type="search" placeholder="Buscar aquí..." autofocus >
-                          <input type="submit" name="buscador" class="boton peque aceptar" value="buscar" onclick="return buscar()">  
-                      </form><br><br>
-                      
-                      <!--=====================================
-        tabla de productos
-        ======================================-->
-                   
-                  <tbody id="tblProductos" >
-                    <?php 
-                    require_once "control/logica/ProductosGet.php";
-                    $datoProductos = Logica\ProductosGet::mostrarProductosPorCategoria();
-                    foreach ($datoProductos as $dato) {
-                        echo "
-                            <div  class='contenedorProductos'>
-                            <ul>
-                            <li>".$dato["nombreProducto"]."</li>
-                                <li><img src='src/assets/productos/".$dato["fotoProducto"]."' width='110px' height:'70px'></li>             
-                                <li> $ ".$dato["precioProducto"]."</li>
-                                    <button class='botonDetalle' data-toggle='modal' data-target='#detalle-modal'>Dellate</button>
-                             
-                        
-                          </ul>
-                             
-                          </div>  "; }
-                    ?>
-                  
-                  
-                  </tbody>
-                  
-                  </table>
-                  
-              
-            
-            
-          
-        </section>
-        <section class="content">
-          <div class="error-page">
-           
-            <div class="error-content">
-              
-              
-            </div>
-          </div>  
-        </section>
-      </div>
+                            alert("buscarCategoria");
+
+                            /* <ul class = "nav navbar-nav">
+                                <li > < a id = "a" href = "InicioProductos" > InicioProductos < /a></li >
+                                </ul> */
+                        }
+                    </script>
+                </div>
+                
+                
+                <!--=====================================
+                            tabla de productos
+                ======================================-->
+                <div class="contenedorProductos">
+                    <form  class="formProductos" action="" name="productos" method="post">
+                        <div class="tablaProductos">
+                            <?php
+                            require_once "control/logica/ProductosGet.php";
+                            $datoProductos = Logica\ProductosGet::mostrarProductosPorCategoria();
+                            foreach ($datoProductos as $dato) {
+                                echo "
+                                    <div class='ul'>
+                                        <div class='li'>" . $dato["nombreProducto"] . "</div>
+                                        <div class='li'><img src='src/assets/productos/" . $dato["fotoProducto"] . "'></div>  
+                                        <div class='li' class='dinerito'> $ " . $dato["precioProducto"] . "</div>
+                                        <div class='li' id='buttonLi'>
+                                            <a id='buttonA'>
+                                                <button class='botonDetalle'data-toggle='modal' data-target='#modalAgregarSucursal'>
+                                                    Dellate
+                                                </button>
+                                            </a>
+                                        </div>
+                                    </div>";
+                            }
+                            echo "<!--<script>
+                                    function enviar(){
+                                    alert('Detalle');
+                                    }
+                                </script> -->"
+                            ?>
+                        </div>
+                    </form>
+                </div>
+
+
+            </section>
+
+
+                <section class="content">
+                    <div class="error-page">
+
+                        <div class="error-content">
+
+                        </div>
+                    </div>
+                </section>
+        </div>
     </div>
 </div>
