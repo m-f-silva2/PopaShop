37f185f24d75d9eb5dae8e1c1a88dfc7604a9848
efom444 <efortiz@unimayor.edu.co>
2019-06-10 09:11:07 -0500

    compra 2

---
+++ views/cliente/BuilderCompra.php
@@ -0,0 +1,20 @@
+<?php
+    //include_once 'clases/class/Producto.php';
+    include_once '../../clases/class/Producto.php';
+    $productos = new clase/prodcuto();
+    
+    
+    //require_once "control/logica/ProductosGet.php";
+    //$datoProductos = Logica\ProductosGet::productoDetalle(1);
+
+
+    /*foreach ($datoProductos as $dato){
+	$this->productos->setNombreProducto($dato["nombreProducto"]);
+    }
+    echo $this->productos->getNombreProducto();
+	*/		/*
+	class BuilderCompra {
+	    //put your code here
+	}
+	*/
+			
